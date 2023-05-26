3b5f0eed229408bc72811ba32393f36087dfecdb
Fábio Assunção <fabio23gt@gmail.com>
2023-05-26 01:54:13 -0300

    Added support for Laravel Livewire

--- src/Traits/Autoloader.php
+++ src/Traits/Autoloader.php
@@ -2,6 +2,12 @@
 
 namespace Codions\ThemesManager\Traits;
 
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
+use Livewire\Component;
+use Livewire\Livewire;
+use ReflectionClass;
+
 trait Autoloader
 {
     public function registerAutoloader()
@@ -16,5 +22,45 @@ trait Autoloader
                 require_once $file;
             }
         });
+    }
+
+    protected function registerLivewireComponents()
+    {
+        $directory = $this->getPath('src/Http/Livewire');
+
+        $namespace = $this->getNamespace('Http\Livewire');
+
+        // If you ever decide to change, I suggest using something like "theme::".
+        $aliasPrefix = '';
+
+        $filesystem = new Filesystem();
+
+        if (! $filesystem->isDirectory($directory)) {
+            return false;
+        }
+
+        $files = collect($filesystem->allFiles($directory));
+
+        $files->map(function ($file) use ($namespace) {
+            return $namespace . '\\' . str_replace(['/', '.php'], ['\\', ''], $file->getRelativePathname());
+        })
+            ->filter(function ($class) {
+                return is_subclass_of($class, Component::class) && ! (new ReflectionClass($class))->isAbstract();
+            })
+            ->each(function ($class) use ($namespace, $aliasPrefix) {
+                $alias = $aliasPrefix . Str::of($class)
+                    ->after($namespace . '\\')
+                    ->replace(['/', '\\'], '.')
+                    ->explode('.')
+                    ->map([Str::class, 'kebab'])
+                    ->implode('.');
+
+                if (Str::endsWith($class, ['\Index', '\index'])) {
+                    Livewire::component(Str::beforeLast($alias, '.index'), $class);
+                }
+
+                // dd($alias, $class);
+                Livewire::component($alias, $class);
+            });
     }
 }
