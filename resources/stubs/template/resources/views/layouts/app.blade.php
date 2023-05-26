423cf14a6761961173fefe6dc17065fbee3637ef
Fábio Assunção <fabio23gt@gmail.com>
2023-05-26 02:40:34 -0300

    Added livewire assets to default layouts

--- resources/stubs/template/resources/views/layouts/app.blade.php
+++ resources/stubs/template/resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
 
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.0/dist/alpine.js" defer></script>
+    @livewireStyles
 </head>
 
 <body class="font-sans antialiased">
@@ -37,6 +38,8 @@
     </div>
 
     @stack('modals')
+
+    @livewireScripts
 </body>
 
 </html>
