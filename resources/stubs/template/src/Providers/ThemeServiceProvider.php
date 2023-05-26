339b9a5908296f7117afb1c4c3222ceda3a1702a
Fábio Assunção <fabio23gt@gmail.com>
2023-05-26 01:52:50 -0300

    Added support for theme service provider

---
+++ resources/stubs/template/src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Themes\DummyNamespace\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
